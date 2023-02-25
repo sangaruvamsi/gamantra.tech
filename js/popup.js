b67a603ef5c7ada3c2abc37151775f9bfb32e5aa
gamantra <126270657+sangaruvamsi@users.noreply.github.com>
2023-02-25 12:17:06 -0500

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+
+        const popup = document.querySelector('.popup');
+        const x = document.querySelector('.popup-content h1')
+
+        window.addEventListener('load', () => {
+            popup.classList.add('showPopup');
+            popup.childNodes[1].classList.add('showPopup');
+        })
+        x.addEventListener('click', () => {
+            popup.classList.remove('showPopup');
+            popup.childNodes[1].classList.remove('showPopup');
+        })
+   
