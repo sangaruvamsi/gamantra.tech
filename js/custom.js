6222272e73b3e5f4eac853df5d013a3d2a6d46e6
gamantra <126270657+sangaruvamsi@users.noreply.github.com>
2023-03-18 06:15:47 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -31,3 +31,15 @@
   })(window.jQuery);
 
 
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
